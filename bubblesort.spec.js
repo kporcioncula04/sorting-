5b25a3730532a649e6e01dc24f45fa3fac061ae8
Karolina <kporcioncula.04@gmail.com>
2020-05-27 13:02:16 -0400

    updated bubblesort

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,18 +1,19 @@
 describe('Bubble Sort', function(){
   let arr = [3, 5, 7, 2]
 
-  // beforeAll( function (){
-  //   spyOn(bubbleSort, 'swap').and.callThrough();
-  // })
-  // it('swapping involves exactly three times', function(){
-  //   bubbleSort.sort();
-  //   expect(bubbleSort.swap.calls.count()).toEqual(3);
-  // })
+  beforeAll( function (){
+    spyOn(bubbleSort, 'sort').and.callThrough();
+  })
   it('handles an empty array', function(){
     expect( bubbleSort.sort([]) ).toEqual( [] );
   });
   it('properly sorts an array', function(){
     expect( bubbleSort.sort(arr)).toEqual([2,3,5,7])
   })
+  it('swapping involves exactly three times', function(){
+    bubbleSort.sort(arr);
+    expect(bubbleSort.sort.calls.count()).toEqual(3);
+  })
+
 });
 
